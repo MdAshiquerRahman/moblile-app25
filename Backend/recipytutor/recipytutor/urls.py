3a3daa0b5a949680f2d3f8ca1aa5d30007277bd5
MdAshiquerRahman <92602508+MdAshiquerRahman@users.noreply.github.com>
2025-04-24 06:08:31 +0000

    added

--- Backend/recipytutor/recipytutor/urls.py
+++ Backend/recipytutor/recipytutor/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings  # Add this
-from django.conf.urls.static import static  # Add this
+from django.urls import path, include, re_path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -17,11 +18,11 @@ urlpatterns = [
     path('api/profile/', include('users.api_urls')),
 ]
 
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-# ======= ADD THIS PART TO HANDLE MEDIA FILES IN DEVELOPMENT ======= #
+# Serve media files in development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # Serve media files in production using django.views.static.serve
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
